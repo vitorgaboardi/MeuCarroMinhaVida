df90ac5b7cd47f70279a7823ebb073c946feff65
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2022-01-23 03:34:07 -0300

    Tela Home completa

---
+++ lib/pages/home.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child: Column(
+              children: [
+                Container(
+                    color: Color.fromARGB(255, 162, 89, 255),
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.fromLTRB(20, 100, 20, 80),
+                    child: Text(
+                      'Meu carro, minha vida',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 80),
+                    )),
+                Row(
+                  children: [
+                    Expanded(
+                        child: SizedBox(
+                          height: 86,
+                          child: Container(
+                              padding: EdgeInsets.fromLTRB(20, 35, 10, 0),
+                              child: ElevatedButton(
+                                  onPressed: null,
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all(Colors.white), // background
+                                      foregroundColor: MaterialStateProperty.all(Colors.white), // foreground
+                                      shape: MaterialStateProperty.all(
+                                          RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(5.0),
+                                              side: BorderSide(color: Color.fromARGB(255, 162, 89, 255))
+                                          )
+                                      )
+                                  ),
+                                  child: Text(
+                                      'ENTRAR',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(255, 162, 89, 255),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w900,
+                                      )
+                                  )
+                              )
+                          )
+                        )
+                    ),
+                    Expanded(
+                        child: SizedBox(
+                          height: 86,
+                          child: Container(
+                              padding: EdgeInsets.fromLTRB(10, 35, 20, 0),
+                              child: ElevatedButton(
+                                  onPressed: null,
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all(Colors.white), // background
+                                      foregroundColor: MaterialStateProperty.all(Colors.white), // foreground
+                                      shape: MaterialStateProperty.all(
+                                          RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(5.0),
+                                              side: BorderSide(color: Color.fromARGB(255, 162, 89, 255))
+                                          )
+                                      )
+                                  ),
+                                  child: Text('REGISTRAR',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(255, 162, 89, 255),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w900,
+                                      )
+                                  ),
+                              )
+                          )
+                        )
+                    )
+                  ]
+                )
+              ],
+            )));
+  }
+}

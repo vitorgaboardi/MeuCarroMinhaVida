4b275182cc96fff796c76c3a5918eb92443fb0f3
vitorgaboardi <vitorgabo@hotmail.com>
2022-01-26 11:00:49 -0300

    Cards - Home page

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -10,21 +10,16 @@ import 'camera.dart';
 import 'profile.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({
-    Key? key,
-    required this.dados
-  }) : super(key: key);
+  const MainPage({Key? key, required this.dados}) : super(key: key);
 
   final dados;
 
   @override
-  State<MainPage> createState() => _MainPage(dados:dados);
+  State<MainPage> createState() => _MainPage(dados: dados);
 }
 
 class _MainPage extends State<MainPage> {
-  _MainPage({
-    required this.dados
-  }) : super();
+  _MainPage({required this.dados}) : super();
 
   final dados;
   late List<CameraDescription> cameras;
@@ -62,17 +57,26 @@ class _MainPage extends State<MainPage> {
       _selectedIndex = index;
 
       if (index == 0) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) => MainPage(dados:dados)));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => MainPage(dados: dados)));
       } else if (index == 1) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) => Search(dados:dados)));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => Search(dados: dados)));
       } else if (index == 2) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) => Camera(camera:camera, dados:dados)));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) =>
+                    Camera(camera: camera, dados: dados)));
       } else if (index == 4) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) => Profile(dados:dados)));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => Profile(dados: dados)));
       }
     });
   }
@@ -98,7 +102,73 @@ class _MainPage extends State<MainPage> {
         backgroundColor: Color.fromARGB(255, 162, 89, 255),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(child: Column(children: [])),
+      body: SingleChildScrollView(
+          child: Column(children: [
+        Card(
+            clipBehavior: Clip.hardEdge,
+            color: Colors.purple[50],
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.arrow_drop_down_circle),
+                  title: const Text('Placa: POX4G21'),
+                  subtitle: Text(
+                    'Gol Prata - 2020',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Image.asset('assets/images/car1.png'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+                  child: Text(
+                    'Data do Furto: 30/11/2021 - 12:29',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                  child: Text(
+                    'Local do Furto: Av. Ayrton Senna, 3022. Neopolis, Natal-RN, 59080-100 - Em frente a farmacia Pague Menos.',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ],
+            )),
+        Card(
+            clipBehavior: Clip.hardEdge,
+            color: Colors.purple[50],
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.arrow_drop_down_circle),
+                  title: const Text('Placa: NNU0B98'),
+                  subtitle: Text(
+                    'Montana Prata - 2020',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Image.asset('assets/images/car2.png'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+                  child: Text(
+                    'Data do Furto: 29/11/2021 - 03:29',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                  child: Text(
+                    'Local do Furto: Av. Xavier da Silveira, 55. Nova Descoberta, Natal-RN, 59015-430 - Em frente a Picanha Churrascaria.',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ],
+            ))
+      ])),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed, // This is all you need!
         items: const <BottomNavigationBarItem>[
